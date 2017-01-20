c5e70cc3bcab19b8f1b8a6301151fa309e5490f4
Adrian Gardian <gardian.adrian@gmail.com>
2017-01-20 20:58:26 +0100

    remove unused files

--- scripts/uploadSymbolsForCrashlyticsIosDebug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#TODO get <IOS_BUILD_FOLDER> parameter from input
-
-poker/libs/ios/CocoaPods/Pods/Fabric/upload-symbols -a caaf994be48eaec0fb5ad04ffb83954f284ff496 -p ios ../build-GameDesire-iphoneos_clang_Qt_5_7_0_for_iOS-Debug/poker/Debug-iphoneos/QtPoker.app.dSYM
